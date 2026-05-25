64c7a767c0dea988b6c640ee345e4424c20beecf
rodrigopaivamcp <rodrigo.mcpaiva@sempreceub.com>
2026-05-25 17:59:14 -0300

    Create carro class - program.cs

---
+++ carro class - program.cs
@@ -0,0 +1,7 @@
+Carro carro1 = new Carro();
+
+carro1.fabricante = "Aston Martin";
+carro1.modelo = "Valhalla";
+carro1.Ano = 1962;
+carro1.qtdPortas = 4;
+Console.WriteLine(carro1.DescricaoDetalhada);
